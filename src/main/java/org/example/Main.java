91ca4bc6b652e8e5964132fe6bc4259a15c2de1f
Anurag <111561835+roarscarch@users.noreply.github.com>
2023-12-29 19:53:01 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,20 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) throws CustomMoodException {
+    public static void main(String[] args) {
         System.out.println("Welcome to mood analyzer.");
 
-        MoodAnalyzer analyzer1 = new MoodAnalyzer("I am in Sad Mood");
-        MoodAnalyzer analyzer2 = new MoodAnalyzer("I am in Any Mood");
-        MoodAnalyzer analyzer3 = new MoodAnalyzer(null);
-        MoodAnalyzer analyzer4 = new MoodAnalyzer("");
+        
+        MoodAnalyzer analyzer1 = new MoodAnalyzer("I am in a Happy Mood");
 
-      
         try {
             System.out.println("Mood is " + analyzer1.analyzeMood());
         } catch (CustomMoodException exception) {
             System.out.println(exception.getMessage());
         }
+
+     
+        MoodAnalyzer analyzer2 = new MoodAnalyzer("I am feeling okay");
 
         try {
             System.out.println("Mood is " + analyzer2.analyzeMood());
@@ -22,14 +22,29 @@ public class Main {
             System.out.println(exception.getMessage());
         }
 
+       
+        MoodAnalyzer analyzer3 = new MoodAnalyzer("Feeling SAD today");
+
         try {
             System.out.println("Mood is " + analyzer3.analyzeMood());
         } catch (CustomMoodException exception) {
             System.out.println(exception.getMessage());
         }
 
+       
+        MoodAnalyzer analyzer4 = new MoodAnalyzer(null);
+
         try {
             System.out.println("Mood is " + analyzer4.analyzeMood());
+        } catch (CustomMoodException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+       
+        MoodAnalyzer analyzer5 = new MoodAnalyzer("");
+
+        try {
+            System.out.println("Mood is " + analyzer5.analyzeMood());
         } catch (CustomMoodException exception) {
             System.out.println(exception.getMessage());
         }
