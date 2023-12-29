5c169f1c9fbcf834c146e4e40732ec033355da4a
Anurag <111561835+roarscarch@users.noreply.github.com>
2023-12-29 19:51:52 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,12 @@ public class Main {
     public static void main(String[] args) throws CustomMoodException {
         System.out.println("Welcome to mood analyzer.");
 
-        // Creating mood analyzer objects
         MoodAnalyzer analyzer1 = new MoodAnalyzer("I am in Sad Mood");
         MoodAnalyzer analyzer2 = new MoodAnalyzer("I am in Any Mood");
         MoodAnalyzer analyzer3 = new MoodAnalyzer(null);
         MoodAnalyzer analyzer4 = new MoodAnalyzer("");
 
-        // checking moods
+      
         try {
             System.out.println("Mood is " + analyzer1.analyzeMood());
         } catch (CustomMoodException exception) {
